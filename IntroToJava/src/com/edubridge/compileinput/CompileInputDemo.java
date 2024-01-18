f94d473c4aa12f920470fded77c182d4352a2ad2
LENOVO <LENOVO@LENOVO-PC-2>
2024-01-18 12:20:10 +0530

    First Commit

---
+++ IntroToJava/src/com/edubridge/compileinput/CompileInputDemo.java
@@ -0,0 +1,26 @@
+package com.edubridge.compileinput;
+
+public class CompileInputDemo {
+
+	public static void main(String[] args) {
+		byte num1=127;
+		short num2=3433;
+		int num3=345671116;
+		long num4=9834567890l;
+		char c='S';
+		String str="EDUBridge";
+		float num5=67.5f;
+		double num6=56.6d;
+		System.out.println(num1);
+		System.out.println(num2);
+		System.out.println(num3);	
+		System.out.println(num4);	
+		System.out.println(num5);
+		System.out.println(num6);
+		System.out.println(c);
+		System.out.println(str);
+		
+
+	}
+
+}
