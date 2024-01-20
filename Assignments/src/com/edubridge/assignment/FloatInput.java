8a347a2dabe6b1acbb4559e632fc7569a7ceedd1
LENOVO <LENOVO@LENOVO-PC-2>
2024-01-20 09:59:49 +0530

    commit four

---
+++ Assignments/src/com/edubridge/assignment/FloatInput.java
@@ -0,0 +1,14 @@
+package com.edubridge.assignment;
+
+import java.util.Scanner;
+public class FloatInput {
+
+	public static void main(String[] args) {
+	Scanner scan= new Scanner(System.in);
+	float x=scan.nextFloat();
+	System.out.printf59.("%.2f",x);
+	scan.close();
+	
+
+	}
+}
---
+++ Assignments/src/com/edubridge/assignment/UnicodeValue.java
@@ -0,0 +1,15 @@
+package com.edubridge.assignment;
+
+public class UnicodeValue {
+
+	public static void main(String[] args) {
+
+		char val='G';
+		int val1=val;
+		System.out.println(val1);
+		char num='g';
+		int va1=num;
+		System.out.println(va1);
+	}
+
+}
